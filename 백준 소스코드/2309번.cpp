33b3136ec3a9a98c19224ffe7f65ee238e4193c3
Antaeseon <ridgnfu@naver.com>
2018-08-31 20:41:11 +0900

    일곱난쟁이

---
+++ 백준 소스코드/2309번.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+
+
+using namespace std;
+
+int main() {
+	int num[9];
+	int sum = 0;
+	for (int i = 0; i < 9; i++) {
+		cin >> num[i];
+		sum += num[i];
+	}
+
+	sort(num, num + 9);
+
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = i+1; j < 9; j++) {
+			if (sum - num[i] - num[j] == 100) {
+				for (int k = 0; k < 9; k++) {
+					if(k!=i&&k!=j)
+						cout << num[k]<<endl;
+				}
+			}
+		}
+	}
+
+
+}
