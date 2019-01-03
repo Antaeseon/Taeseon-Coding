633906168f22a31235c0785f398659210596dfe5
Antaeseon <ridgnfu@naver.com>
2019-01-03 15:40:16 +0900

    Tanos

---
+++ 백준 소스코드/15802번.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	string inp;
+	cin >> inp;
+
+	if (inp == "ats412")
+		cout << "0" << "\n";
+	else
+		cout << "1" << "\n";
+}
