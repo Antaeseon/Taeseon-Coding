9a02ebcf8629d7ddd0289fa18becf83997ab72ea
Antaeseon <ridgnfu@naver.com>
2019-01-04 18:39:57 +0900

    star-16

---
+++ 백준 소스코드/10991번.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 1; j < N - i; j++)cout << " ";
+		cout << "*";
+		if (i == 0) {
+			cout << endl;
+			continue;
+		}
+		for (int j = 0; j < 2 * i - 1; j++) {
+			if (j % 2 == 1)
+				cout << "*";
+			else {
+				cout << " ";
+			}
+		}
+		cout << "*";
+		cout << endl;
+	}
+}
