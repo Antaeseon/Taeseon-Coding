ebe0dbe100d0ff4b7fc0d7f222e884ddb9a3ba82
Antaeseon <ridgnfu@naver.com>
2019-03-07 00:35:30 +0900

    taxi

---
+++ 백준 소스코드/3053번.cpp
@@ -0,0 +1,13 @@
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+#define PI 3.14159265358979323846
+
+using namespace std;
+
+int main() {
+	double n;
+	scanf("%lf", &n);
+	printf("%6lf\n", n*n*PI);
+	printf("%6lf\n", n*n * 2);
+}
