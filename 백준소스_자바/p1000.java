5fea9b225d12e7873ab9040395d3feeaf600b515
Antaeseon <ridgnfu@naver.com>
2020-05-02 13:27:01 +0900

    java 기초문제

---
+++ 백준소스_자바/p1000.java
@@ -0,0 +1,17 @@
+package น้มุ;
+
+import java.util.Scanner;
+
+public class p1000 {
+
+	public static void main(String[] args) {
+		int a,b;
+		Scanner sc = new Scanner(System.in);
+		
+		a= sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a+b);
+		
+	}
+
+}
---
+++ 백준소스_자바/p1008.java
@@ -0,0 +1,14 @@
+package น้มุ;
+
+import java.util.Scanner;
+
+public class p1008 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double a,b;
+		a = sc.nextDouble();
+		b = sc.nextDouble();
+		System.out.println(a/b);
+	}
+}
