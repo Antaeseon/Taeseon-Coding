f5173e8322a0e847c1f0362ca348a35b84e4c1f1
Antaeseon <ridgnfu@naver.com>
2020-06-30 23:26:58 +0900

    n and m no.2

---
+++ 백준 소스코드/15650번.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+bool visited[10];
+int arr[10];
+int temp;
+
+void dfs(int index,int cnt)
+{
+    if (index == m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+
+    for (int i = cnt+1; i <= n; i++)
+    {
+        if (!visited[i])
+        {
+            visited[i] = true;
+            arr[index] = i;
+            dfs(index + 1,i); 
+            visited[i] = false;
+        }
+    }
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false), cin.tie(0);
+    cin >> n >> m; // ex n = 3, m =1
+    dfs(0,0);
+}
